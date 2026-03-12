a25b732ce490cc35ee0849e56e41451ff691cee8
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-03-12 15:13:52 +0530

    [Sameer]: UC2 : Feet and Inches equality

--- src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,4 +1,3 @@
-
 package com.quantitymeasurement;
 
 public class QuantityMeasurementApp {
@@ -24,13 +23,45 @@ public class QuantityMeasurementApp {
         }
     }
 
-    public static void main(String[] args) {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
-        System.out.println("Comparing 1.0 ft and 1.0 ft: " + feet1.equals(feet2));
+    public static class Inches {
+        private final double value;
 
-        Feet feet3 = new Feet(1.0);
-        Feet feet4 = new Feet(2.0);
-        System.out.println("Comparing 1.0 ft and 2.0 ft: " + feet3.equals(feet4));
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches inches = (Inches) obj;
+            return Double.compare(this.value, inches.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    // Static method for Feet equality check
+    public static boolean checkFeetEquality(double value1, double value2) {
+        Feet feet1 = new Feet(value1);
+        Feet feet2 = new Feet(value2);
+        return feet1.equals(feet2);
+    }
+
+    // Static method for Inches equality check
+    public static boolean checkInchesEquality(double value1, double value2) {
+        Inches inches1 = new Inches(value1);
+        Inches inches2 = new Inches(value2);
+        return inches1.equals(inches2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Comparing 1.0 ft and 1.0 ft: " + checkFeetEquality(1.0, 1.0));
+        System.out.println("Comparing 1.0 ft and 2.0 ft: " + checkFeetEquality(1.0, 2.0));
+        System.out.println("Comparing 1.0 inch and 1.0 inch: " + checkInchesEquality(1.0, 1.0));
+        System.out.println("Comparing 1.0 inch and 2.0 inch: " + checkInchesEquality(1.0, 2.0));
     }
 }
--- src/test/java/com/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,4 +1,3 @@
-
 package com.quantitymeasurement;
 
 import org.junit.jupiter.api.Test;
@@ -6,7 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuantityMeasurementAppTest {
 
-    // Test 1: Same value - should be equal
+    // -------------------- FEET TESTS (UC1) --------------------
+
     @Test
     public void givenTwoFeetValues_WhenBothAreOneFoot_ThenShouldBeEqual() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
@@ -14,7 +14,6 @@ public class QuantityMeasurementAppTest {
         assertEquals(feet1, feet2, "1.0 ft should equal 1.0 ft");
     }
 
-    // Test 2: Different values - should not be equal
     @Test
     public void givenTwoFeetValues_WhenValuesAreDifferent_ThenShouldNotBeEqual() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
@@ -22,14 +21,12 @@ public class QuantityMeasurementAppTest {
         assertNotEquals(feet1, feet2, "1.0 ft should not equal 2.0 ft");
     }
 
-    // Test 3: Null comparison - should return false
     @Test
     public void givenFeetValue_WhenComparedWithNull_ThenShouldNotBeEqual() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
         assertNotEquals(feet1, null, "1.0 ft should not equal null");
     }
 
-    // Test 4: Different type comparison - should return false
     @Test
     public void givenFeetValue_WhenComparedWithNonFeetType_ThenShouldNotBeEqual() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
@@ -37,10 +34,44 @@ public class QuantityMeasurementAppTest {
         assertNotEquals(feet1, nonNumeric, "Feet should not equal a String type");
     }
 
-    // Test 5: Same reference - reflexive property
     @Test
     public void givenFeetValue_WhenComparedWithItself_ThenShouldBeEqual() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
         assertEquals(feet1, feet1, "Feet object should equal itself");
+    }
+
+    // -------------------- INCHES TESTS (UC2) --------------------
+
+    @Test
+    public void givenTwoInchValues_WhenBothAreOneInch_ThenShouldBeEqual() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(1.0);
+        assertEquals(inches1, inches2, "1.0 inch should equal 1.0 inch");
+    }
+
+    @Test
+    public void givenTwoInchValues_WhenValuesAreDifferent_ThenShouldNotBeEqual() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(2.0);
+        assertNotEquals(inches1, inches2, "1.0 inch should not equal 2.0 inch");
+    }
+
+    @Test
+    public void givenInchValue_WhenComparedWithNull_ThenShouldNotBeEqual() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        assertNotEquals(inches1, null, "1.0 inch should not equal null");
+    }
+
+    @Test
+    public void givenInchValue_WhenComparedWithNonInchType_ThenShouldNotBeEqual() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        String nonNumeric = "1.0";
+        assertNotEquals(inches1, nonNumeric, "Inches should not equal a String type");
+    }
+
+    @Test
+    public void givenInchValue_WhenComparedWithItself_ThenShouldBeEqual() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        assertEquals(inches1, inches1, "Inches object should equal itself");
     }
 }
